fbc8aed56bfc26fd724eeb3125d5f1d58e925dee
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-11-23 10:37:58 +0100

    get all CPT in single.php :wrench:

--- wp-content/themes/climate-theme/single.php
+++ wp-content/themes/climate-theme/single.php
@@ -10,15 +10,15 @@
  */
 
 $context = Timber::get_context();
-$post = Timber::query_post();
+$post = new TimberPost();
 $context['post'] = $post;
-$post_slug = $post->post_name; //get slug of current page
+/*$post_slug = $post->post_name; //get slug of current page
 $articles = new WP_Query( array( 'tag' => $post_slug ) );//get articles corresponding to page called
-$context['articles'] = $articles; //return articles to context
+$context['articles'] = $articles; //return articles to context*/
 $permalink = get_permalink();
 
 switch ( true ) {
-    case strpos($permalink, 'article'):
+    case strpos($permalink, 'articles'):
         Timber::render( 'single-article.twig', $context );
         break;
 
