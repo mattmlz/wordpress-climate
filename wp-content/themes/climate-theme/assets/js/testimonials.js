c94ce17d80c3db119efa0b8cb8f74165b15f0803
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-11-24 16:14:23 +0100

    page testimonials :zap: :art:

--- wp-content/themes/climate-theme/assets/js/testimonials.js
+++ wp-content/themes/climate-theme/assets/js/testimonials.js
@@ -2,16 +2,20 @@ let $url = document.location.href;
 
 if( $url.match('testimonials')) {
     // define our colors
-    const colors = ["#52A55D","#047713","#F5A623", "#FFFD59"]
+    const colors = ["#52A55D","#047713","#F5A623", "#D5D317"]
 
     // get all of our images within a specified container element
-    let $images = document.querySelectorAll('.testimonial')
+    let $testimonials = document.querySelectorAll('.testimonial')
 
-    for (let i=0; i < $images.length; i++) {
+    for (let i=0; i < $testimonials.length; i++) {
         // get a random color from list
         let random_color = Math.floor(Math.random()*colors.length)
 
+        //get testimonial category
+        let $category = $testimonials[i].querySelector('.testimonial-category')
+
         // apply random color to current image
-        $images[i].style.backgroundColor = colors[random_color];
+        $testimonials[i].style.borderColor = colors[random_color]
+        $category.style.color = colors[random_color]
     }
 }
